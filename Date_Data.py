f6a431be771b0a7e2eec4a11780315b388967c26
Nodar Svanidze <99223872+nodarisvanidze88@users.noreply.github.com>
2023-04-01 15:04:03 +0400

    Create Date_Data.py

---
+++ Date_Data.py
@@ -0,0 +1,52 @@
+def month_for_Outdated():
+    # Combine short and long month names
+    monthss = {
+        "january": "01",
+        "jan": "01",
+        "february": "02",
+        "feb": "02",
+        "march": "03",
+        "mar": "03",
+        "april": "04",
+        "apr": "04",
+        "may":"05",
+        "june":"06",
+        "jun":"06",
+        "july":"07",
+        "jul":"07",
+        "august":"08",
+        "aug":"08",
+        "september":"09",
+        "sep":"09",
+        "october":"10",
+        "oct":"10",
+        "november":"11",
+        "nov":"11",
+        "december":"12",
+        "dec":"12"
+    }
+    return monthss
+
+def dates_for_Outdated():
+    # Combine day number with 0 
+    dates = {
+        "01": "01",
+        "1": "01",
+        "02": "02",
+        "2": "02",
+        "03": "03",
+        "3": "03",
+        "04": "04",
+        "4": "04",
+        "05":"05",
+        "5":"05",
+        "06":"06",
+        "6":"06",
+        "07":"07",
+        "7":"07",
+        "08":"08",
+        "8":"08",
+        "09":"09",
+        "9":"09",
+    }
+    return dates
